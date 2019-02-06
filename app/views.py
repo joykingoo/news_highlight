30b12ebe526961a11b2e7bdc76a1d247711afb73
joy <wendokingoo@gmail.com>
2019-02-06 14:46:56 +0300

    Instance folder added with config file and added api key

--- app/views.py
+++ app/views.py
@@ -4,12 +4,13 @@ from app import app
 # Views
 @app.route('/')
 def index():
-
+    
     '''
     View root page function that returns the index page and its data
     '''
-    message = 'Hello World'
-    return render_template('index.html',message = message)
+
+    title = 'Home - Welcome to The best News Website Online'
+    return render_template('index.html', title = title)
 
 @app.route('/news/<news_id>')
 def news(news_id):
