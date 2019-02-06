c7d6fb4830925de8d141a1762bae58c20abd3492
joy <wendokingoo@gmail.com>
2019-02-06 14:38:35 +0300

    __init__.py file modified

--- app/views.py
+++ app/views.py
@@ -8,4 +8,13 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    message = 'Hello World'
+    return render_template('index.html',message = message)
+
+@app.route('/news/<news_id>')
+def news(news_id):
+
+    '''
+    View news page function that returns the news details page and its data
+    '''
+    return render_template('news.html',id = news_id)
