a57093f8fdd86bc13b5ef18e8afb033a46f6583d
joy <wendokingoo@gmail.com>
2019-02-06 19:50:26 +0300

    news.py and news_test.py files created and modified

---
+++ app/models/news.py
@@ -0,0 +1,10 @@
+class News:
+    '''
+    Movie class to define Movie Objects
+    '''
+
+    def __init__(self,id,name):
+        self.id =id
+        self.name = name
+       
+    
--- app/views.py
+++ app/views.py
@@ -4,18 +4,17 @@ from app import app
 # Views
 @app.route('/')
 def index():
-    
+
     '''
     View root page function that returns the index page and its data
     '''
-
-    title = 'Home - Welcome to The best News Website Online'
-    return render_template('index.html', title = title)
+    return render_template('index.html')
 
 @app.route('/news/<news_id>')
 def news(news_id):
 
     '''
-    View news page function that returns the news details page and its data
+    View movie page function that returns the movie details page and its data
     '''
-    return render_template('news.html',id = news_id)
+    title = 'Home - Welcome to The best News Website '
+    return render_template('news.html', title = title)
