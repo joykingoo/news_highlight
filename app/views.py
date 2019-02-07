1cd6652d37e86ca64402bf283b2a8b72699dc30f
joy <wendokingoo@gmail.com>
2019-02-07 12:36:42 +0300

    Macros file created and modifed alongside views and index files respectively

--- app/views.py
+++ app/views.py
@@ -15,8 +15,11 @@ def index():
      # Getting entertainment news
     entertainment_news = get_news('entertainment')
     print(entertainment_news)
+    # Getting entertainment news
+    science_news=get_news('science')
+    print (science_news)
     title = 'Home - Welcome to The best News Website Online'
-    return render_template('index.html', name = title,general = general_news, entertainment =entertainment_news)
+    return render_template('index.html', name = title,general = general_news, entertainment =entertainment_news, science =science_news)
 
 @app.route('/news')
 def news():
