f91b3bfa2c378026215f2b57112b438082051805
joy <wendokingoo@gmail.com>
2019-02-07 10:22:28 +0300

    view file modified with imports from request file

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_news
 
 # Views
 @app.route('/')
@@ -8,10 +9,14 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    # Getting popular news
+    popular_news = get_news('popular')
+    print(popular_news)
+    title = 'Home - Welcome to The best News Website Online'
+    return render_template('index.html', name = title,popular = popular_news)
 
-@app.route('/news/<news_id>')
-def news(news_id):
+@app.route('/news')
+def news():
 
     '''
     View movie page function that returns the movie details page and its data
